b53e2e02be60fc4a1284467754b4ba306caf94da
Hae Jun Kim <dan.kimhaejun@gmail.com>
2019-12-16 16:00:12 +0900

    Fail test

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # AX_CodingTest
+
 Test code with AX
+
+1. HackerNews(https://news.ycombinator.com/) 에 있는 글 목록을 가져와 출력하는 프로그램을 작성하세요.
+2. 웹 크롤링 경험이 없다면 구체적으로 어떤 과정을 거쳐 어떤 값을 이용하면 원하는 결과를 얻을 수 있을지 서술해주세요.
+
+글 목록은 원 글과 동일한 순서여야 합니다.
+글 목록은 제목과 원글 링크가 있어야 합니다.
+
+1. getElement 를 통한 tbody의 전체 내용을 배열형태로 가져온다.
+2. 하나의 글 내용이 class="athing"에 속해 있다.
+
+- 핵심은 글 제목과 원글 링크이므로 배열을 순차적으로 돌아서
+- class="rank" 에서 순서를 가져오고
+- class="title" 내의 a href 링크에서 링크를 크롤링하고, value(제목) 을 크롤링하여 하나의 객체로 만드는 함수를 작성한다.
+  [
+  {
+  rank : 1,
+  link : 'https://mystery.knightlab.com/',
+  title : 'SQL Murder Mystery'
+  },
+  {
+  rank : 2,
+  link : 'https......',
+  title : 'SQL ....'
+  },
+  ......
+  ]
+
+  크롤링이라면 tbody를 전체 배열로 가져와서 각 class="athing"인 인자들을 순차적으로 돌아 원하는 값을 가져올 것 같습니다.
---
+++ openDoor.js
@@ -0,0 +1,44 @@
+const openDoor = (doors, people) => {
+  let doorsArr = new Array(doors + 1).fill(false);
+  console.log('doorsArr', doorsArr);
+  console.log('doorsArr.length', doorsArr.length);
+  for (let i = 1; i < people; i++) {
+    for (let j = i + i; j <= doors; j = j + i) {
+      console.log('doorsArr[j]', doorsArr[j]);
+      if (doorsArr[j] === true) {
+        doorsArr[j] = false;
+      } else {
+        doorsArr[j] = true;
+      }
+    }
+  }
+  console.log('doorsArr', doorsArr);
+};
+console.log(openDoor(50, 1));
+// const openDoor = function(doors, people) {
+//   let doorsArr = new Array(doors + 1).fill(false);
+//   let person = 1;
+//   let makeTrue = (doors, people, i) => {
+//     if (i <= people) {
+//       if (doorsArr[i] === true) {
+//         doorsArr[i] = false;
+//       } else {
+//         doorsArr[i] = true;
+//       }
+//       person++;
+//       makeTrue(doors, people, i + 1);
+//       console.log('i', i);
+//     }
+//     while (i <= people) {
+//       if (doorsArr[i] === true) {
+//         doorsArr[i] = false;
+//       } else {
+//         doorsArr[i] = true;
+//       }
+//       i = i + person;
+//     }
+//   };
+//   console.log('doorsArr', doorsArr);
+//   makeTrue(doors, people, 1);
+//   console.log('doorsArr', doorsArr);
+// };
